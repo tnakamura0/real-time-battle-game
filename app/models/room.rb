2d8f12600460060dc6426d115602bc97424e680f
t.nakamura <206074491+tnakamura0@users.noreply.github.com>
2026-02-24 21:14:46 +0900

    feat: Roomモデルにバリデーションを追加

--- app/models/room.rb
+++ app/models/room.rb
@@ -1,3 +1,11 @@
 class Room < ApplicationRecord
+  enum status: { waiting: 0, playing: 1, finished: 2 }
+  
+  validates :token, presence: true
+  validates :passcord, presence: true
+  validates :initial_hp, presence: true
+  validates :guard_cooldown_turns, presence: true
+  validates :status, presence: true
+
   belongs_to :owner, class_name: "User"
 end
