3f86f73a8a3999d92cd7e2750ffdf0e8644ebd25
t.nakamura <206074491+tnakamura0@users.noreply.github.com>
2026-02-24 21:17:25 +0900

    feat: roomsテーブルにDBレベルの制約を追加

--- db/migrate/20260224013424_create_rooms.rb
+++ db/migrate/20260224013424_create_rooms.rb
@@ -1,14 +1,17 @@
 class CreateRooms < ActiveRecord::Migration[8.1]
   def change
     create_table :rooms do |t|
-      t.string :token
-      t.string :passcord
-      t.integer :initial_hp
-      t.integer :guard_cooldown_turns
-      t.integer :status
+      t.string :token, null: false
+      t.string :passcord, null: false
+      t.integer :initial_hp, default: 3, null: false
+      t.integer :guard_cooldown_turns, default: 1, null: false
+      t.integer :status, default: 0, null: false
       t.references :owner, null: false, foreign_key: { to_table: :users }
 
       t.timestamps
+
+      t.index :token
+      t.index :passcord
     end
   end
 end
