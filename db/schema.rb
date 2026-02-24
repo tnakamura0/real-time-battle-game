d1d012547fae2996a503e8f21e666897c9bedbf7
t.nakamura <206074491+tnakamura0@users.noreply.github.com>
2026-02-24 21:17:52 +0900

    feat: マイグレーションを適用

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.1].define(version: 2026_02_11_062715) do
+ActiveRecord::Schema[8.1].define(version: 2026_02_24_013424) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
@@ -42,6 +42,20 @@ ActiveRecord::Schema[8.1].define(version: 2026_02_11_062715) do
     t.index ["blob_id", "variation_digest"], name: "index_active_storage_variant_records_uniqueness", unique: true
   end
 
+  create_table "rooms", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.integer "guard_cooldown_turns", default: 1, null: false
+    t.integer "initial_hp", default: 3, null: false
+    t.bigint "owner_id", null: false
+    t.string "passcord", null: false
+    t.integer "status", default: 0, null: false
+    t.string "token", null: false
+    t.datetime "updated_at", null: false
+    t.index ["owner_id"], name: "index_rooms_on_owner_id"
+    t.index ["passcord"], name: "index_rooms_on_passcord"
+    t.index ["token"], name: "index_rooms_on_token"
+  end
+
   create_table "users", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.string "email", default: "", null: false
@@ -60,4 +74,5 @@ ActiveRecord::Schema[8.1].define(version: 2026_02_11_062715) do
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
+  add_foreign_key "rooms", "users", column: "owner_id"
 end
