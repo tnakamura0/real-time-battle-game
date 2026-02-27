73c7de9ae0d8e4bcf90c0be37183ff24077ad963
t.nakamura <206074491+tnakamura0@users.noreply.github.com>
2026-02-27 11:07:12 +0900

    feat: Roomユーザー間でのAction Cable接続を実装

--- app/channels/room_channel.rb
+++ app/channels/room_channel.rb
@@ -1,6 +1,13 @@
 class RoomChannel < ApplicationCable::Channel
   def subscribed
     # stream_from "some_channel"
+    room = Room.find_by(token: params[:token])
+
+    if room
+      stream_for room
+    else
+      reject
+    end
   end
 
   def unsubscribed
